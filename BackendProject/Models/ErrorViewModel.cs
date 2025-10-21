62851971151807d4073ae7aa679d32c75b3a1ad7
JepMach <jeppea@ruc.dk>
2025-10-21 11:10:49 +0200

    Created ASP.NET Web Core (MVC) Project

---
+++ BackendProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BackendProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
