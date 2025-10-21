905394a31cd5d8d1bf1a650f76fadbb20e0e3b97
pBlem <petersblem@birman.dk>
2025-10-21 16:38:34 +0200

    Add Year4 class placeholder

---
+++ BackendSolution/DataService/Util/Year4.cs
@@ -0,0 +1,6 @@
+namespace DataService.Util; // WARN: might need to just be `namespace DataService.Entities;` or `namespace DataService;`. Unsure.
+
+public class Year4
+{
+    // FIX: The datateam should figure out what a C# reprensentation of the year4 from the db is
+}
