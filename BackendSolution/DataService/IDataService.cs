725f2f4c22621b8fb7f38d25666a8aad866e5ce2
pBlem <petersblem@birman.dk>
2025-10-21 16:38:48 +0200

    Add IDataService interface definition
    Introduces the IDataService interface in the DataService namespace, outlining placeholders for Page, UserAction, and User related methods to be implemented.

---
+++ BackendSolution/DataService/IDataService.cs
@@ -0,0 +1,31 @@
+namespace DataService
+{
+    public interface IDataService
+    {
+        //////////
+        // Page
+        //
+        // Title : Page
+        // Plot
+        //
+        // Individual : Page
+        //
+        // Contribution
+        //
+
+        //////////
+        // UserAction
+        //
+        // Visit : UserAction
+        //
+        // Search : UserAction
+        //
+        // Bookmark : UserAction
+        //
+        // Rating : UserAction
+        //
+
+        //////////
+        // User
+    }
+}
