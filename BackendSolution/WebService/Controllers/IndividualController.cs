e728e3cafdce55ad96e72d4474dfed6ff6380c1e
Emilie <emiliebrandstrup@gmail.com>
2025-10-31 13:50:39 +0100

    Top10 individuals ordered by name
    Added method to show a list of individuals sorted by name. We might want to add the possibility of sorting the list by the individual's rating - i made a switch which it can be added into.

--- BackendSolution/WebService/Controllers/IndividualController.cs
+++ BackendSolution/WebService/Controllers/IndividualController.cs
@@ -4,8 +4,10 @@ using AutoMapper;
 using DataService.Entities;
 using DataService.DTOs;
 using DataService.Data;
+using AutoMapper.QueryableExtensions; // For ProjectTo in GetTop10
 
 namespace WebService.Controllers;
+
 [ApiController]
 [Route("api/individual")]
 public class IndividualController : ControllerBase
@@ -19,8 +21,14 @@ public class IndividualController : ControllerBase
         _context = context;
         _mapper = mapper;
     }
+
+
+
+
+
+    // Endpoint to get full details of an individual (by tconst)
     [HttpGet("{tconst}")]
-    public async Task<ActionResult<IndividualFullDTO>> Get(string tconst)
+    public async Task<ActionResult<IndividualFullDTO>> GetIndividual(string tconst)
     {
         var individual = await _context.Individuals
             .FirstOrDefaultAsync(i => i.Iconst == tconst);
@@ -33,6 +41,7 @@ public class IndividualController : ControllerBase
         return Ok(_mapper.Map<IndividualFullDTO>(individual));
     }
 
+    // Endpoint to get reference details of an individual (by tconst)
     [HttpGet("reference/{tconst}")]
     public async Task<ActionResult<IndividualReferenceDTO>> GetReference(string tconst)
     {
@@ -46,4 +55,40 @@ public class IndividualController : ControllerBase
 
         return Ok(_mapper.Map<IndividualReferenceDTO>(individual));
     }
+
+    // Endpoint to get top 10 individuals - sorted by name but we might include rating?
+    [HttpGet("top10")]
+    public async Task<ActionResult<List<IndividualReferenceDTO>>> GetTop10([FromQuery] string? orderBy = "name")
+    {
+        var query = _context.Individuals.AsQueryable();
+
+        query = orderBy?.ToLower() switch
+        {
+        "name" => query.OrderBy(i => i.Name),
+            /*
+            "rating" => query
+                .Include(i => i.Ratings)
+                .OrderByDescending(i => i.Ratings.Any() ? i.Ratings.Average(r => r.AverageRating) : 0),
+            "rating_desc" => query
+                .Include(i => i.Ratings)
+                .OrderByDescending(i => i.Ratings.Any() ? i.Ratings.Average(r => r.AverageRating) : 0),
+            */
+            _ => query.OrderBy(i => i.Name) // default
+           
+        };
+
+        var individuals = await query
+            .Take(10)
+            .ProjectTo<IndividualReferenceDTO>(_mapper.ConfigurationProvider)
+            .ToListAsync();
+
+        if (!individuals.Any())
+        {
+            return NotFound(new { message = "No individuals found." });
+        }
+
+        return Ok(individuals);
+    }
+
+
 }
