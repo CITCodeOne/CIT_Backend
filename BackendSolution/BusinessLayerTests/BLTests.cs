8e38baa4a4dff7cd9779ad425b81220df999ed17
Emilie <emiliebrandstrup@gmail.com>
2025-11-05 12:04:56 +0100

    Working on businesslayertests

--- BackendSolution/BusinessLayerTests/BLTests.cs
+++ BackendSolution/BusinessLayerTests/BLTests.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 public class BusinessLayerTests
 {
+    private CITContext DbContext()
+    {
+        var options = new DbContextOptionsBuilder<CITContext>()
+            .UseNpgsql("Host=your_host;Database=your_database;Username=your_user;Password=your_password") // PostgreSQL eksempel
+            .Options;
+
+        return new CITContext(options);
+    }
+
+    private IMapper CreateMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        return config.CreateMapper();
+    }
+
     [Fact]
     public async Task ExampleBusinessLogicTest()
     {
